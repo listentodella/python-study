8e7dbcb29fc7d8964727e97bd185d86e44a25763
listentodella <937138688@qq.com>
2019-10-07 23:11:42 +0800

    base: input/ouput & calc

---
+++ base.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+# coding=utf-8
+#print("please input a num:")
+#NOTE: input() returns a str 
+#a = input()
+a = eval(input("please input a number:"))
+if (a >= 0):
+    print("fabs(a) =", a)
+else:
+    print("fabs(a) =", -a)
+
+print("this is python\nthis is python\nthis is python")
+print("this is python...this is python...this is python")
+print("this is python ... this is python ... this is python")
+print('''this is python
+... this is python
+... this is python''') # so, ... only used in cmdline
+
+
+# in python, var could be any valid types
+a = 123
+print("a =", a)
+a = "abc"
+print("a =", a)
+
+print("10 / 3 =", 10 / 3)
+print("10 // 3 =", 10 // 3)
+print("9 / 3 =", 9 / 3)
+print("9 // 3 =", 9 // 3)
+
--- calc.py
+++ calc.py
@@ -1,3 +1,9 @@
 a = 100 + 200 + 300
 b = 2 ** 10
 print("a = ", a, "b = ", b)
+
+print("please input your name:")
+name = input()
+print("please input your age:")
+age = input()
+print("your name is", name, ", age is", age)
